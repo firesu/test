c93b1c387eb2d4f875544718eb77b0eeec75df9e
firesu <firesu123@yahoo.com.tw>
2015-10-28 13:48:48 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,6 @@
 ＜script type="text/javascript"＞
-jQuery(document).ready(function($)
-{
   if ($(“img”).attr(“src”)==”images/nextstep.png”) 
   {
    alert(document.forms[2].name);
   }
-}
 ＜/script＞
