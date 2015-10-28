1090d35ef4c416814c3182190ad0650e5dc56e73
firesu <firesu123@yahoo.com.tw>
2015-10-28 15:59:57 +0800

    Update test.js

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($)
     {
       window.scroll(0,100);
       document.forms[2].elements[5].click();
-      $("input[@type=radio]").attr("checked",'4');
+      sco_check_login('ips_connect');
     }
   }
 }
