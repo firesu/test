06ff589199c3cf7eda8b1e72a7616c90c8f2f9da
firesu <firesu123@yahoo.com.tw>
2015-10-28 15:56:44 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,10 @@
 jQuery(document).ready(function($)
 //jQuery(window).load(function()
 {
-  if ($("img").attr("src")=="images/nextstep.png")
-  {
-    window.scroll(0,100);
-    alert("123");
-  }
   var frm = $('form[name="formSCO"]');
   if (frm.length >0)
   {
-    if ($(this).complete)
-    {
-      alert("formSCO 完成");
-    }
+
   }
   for (i = 0 ; i < document.images.length ; i++)
   {
