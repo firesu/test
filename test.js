e8100ab6e52a50d158e58e7691ae8f9118402c20
firesu <firesu123@yahoo.com.tw>
2015-10-28 16:58:28 +0800

    Update test.js

--- test.js
+++ test.js
@@ -18,6 +18,7 @@ jQuery(document).ready(function($)
       {
         document.forms[2].elements[11].click();
         setTimeout(window.scroll(0,100), 300);
+        setTimeout(sco_check_login('ips_connect'), 300);
       //Submits form immediately after image loaded.
       //$('#myForm').submit();  
       // Submits form 1 second after image has loaded
