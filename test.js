4d5f2b5fbeb96ebe30e602db40585faebe0ea1e5
firesu <firesu123@yahoo.com.tw>
2015-10-28 17:14:03 +0800

    Update test.js

--- test.js
+++ test.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function($)
       {
         document.forms[2].elements[11].click();
         //setTimeout(window.scroll(0,100), 300);
-        setTimeout(image.click(), 600);
+        setTimeout(submit_payment(ips_connect,1), 600);
       //Submits form immediately after image loaded.
       //$('#myForm').submit();  
       // Submits form 1 second after image has loaded
