90f249fa10f6e9ecdb3b92d8f78664e393ab7c8b
firesu <firesu123@yahoo.com.tw>
2015-10-28 13:38:09 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+＜script type="text/javascript"＞
+jQuery(document).ready(function($)
+{
+  if ($(“img”).attr(“src”)==”images/nextstep.png”) 
+  {
+   alert(document.forms[2].name);
+  }
+}
+＜/script＞
