73f8140160d11aac99253e9766fee4628fba8401
firesu <firesu123@yahoo.com.tw>
2015-10-28 15:30:21 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 jQuery(document).ready(function($)
 {
-if ($("img").attr("src")=="images/nextstep.png")
-{
-window.scroll(0,100);
-alert("123");
-}
+  if ($("img").attr("src")=="images/nextstep.png")
+  {
+    window.scroll(0,100);
+    alert("123");
+  }
+  var frm = $('form[name="formSCO"]');
+  if (frm.length >0)
+  {
+    alert("formSCO存在");
+  }
 });
