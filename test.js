cd52378749c4c7d12b5be17b46871f8c80b9f4d2
firesu <firesu123@yahoo.com.tw>
2015-10-29 11:30:16 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
 $(document).ready(function($)
 //jQuery(window).load(function()
 {
-  //alert(document.body.scrollHeight);
+  alert(document.body.scrollHeight);
 
   for (i = 0 ; i < document.images.length ; i++)
   {
     if (document.images[i].src.match("nextstep.png"))
     {
-      //window.scroll(0,100);
-      //document.forms[2].elements[11].click();
-      //sco_check_login('ips_connect');
       alert(document.body.scrollHeight);
       var image = document.images[i];
       $(image).load(function()
