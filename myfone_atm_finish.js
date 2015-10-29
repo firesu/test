4cd502ec0e139184392a576f64ddf75fbb6030d9
firesu <firesu123@yahoo.com.tw>
2015-10-29 13:21:57 +0800

    Update myfone_atm_finish.js

--- myfone_atm_finish.js
+++ myfone_atm_finish.js
@@ -7,11 +7,16 @@ $(document).ready(function($)
     {
      var checkExist = setInterval(function()
      {
-      if (document.body.scrollHeight>6580)
+      if (document.body.scrollHeight>750)
      {
       //alert(document.body.scrollHeight);
-      setTimeout(document.forms[2].elements[11].click(), 150);
-      setTimeout(function(){submit_payment('ips_connect',1)}, 300);
+      //setTimeout(document.forms[2].elements[11].click(), 150);
+      //setTimeout(function(){submit_payment('ips_connect',1)}, 300);
+      var image = document.images[i];
+      $(image).load(function()
+      {
+        setTimeout(image.click(), 100);
+      });      
       clearInterval(checkExist);
      }
     }, 100);
