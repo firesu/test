94a90083611947f75439d54d999454a45fcc727a
firesu <firesu123@yahoo.com.tw>
2015-10-29 16:10:18 +0800

    Update myfone_atm.js

--- myfone_atm.js
+++ myfone_atm.js
@@ -22,8 +22,8 @@ $(document).ready(function($)
      }
     }, 100);
     */
-    alert('find formSCO form');
-    alert(document.body.scrollHeight);
+    //alert('find formSCO form');
+    //alert(document.body.scrollHeight);
     for (i = 0 ; i < document.images.length ; i++)
     {
       if (document.images[i].src.match("nextstep.png"))
@@ -33,8 +33,8 @@ $(document).ready(function($)
         if (document.body.scrollHeight>6580)
        {
         //alert(document.body.scrollHeight);
-        setTimeout(document.forms[2].elements[11].click(), 150);
-        setTimeout(function(){submit_payment('ips_connect',1)}, 300);
+        setTimeout(document.forms[2].elements[11].click(), 200);
+        setTimeout(function(){submit_payment('ips_connect',1)}, 450);
         clearInterval(checkExist);
        }
       }, 100);
