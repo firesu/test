75aae8104f9f12623d04b028dfd472eef660f4ea
firesu <firesu123@yahoo.com.tw>
2015-10-29 08:49:17 +0800

    Update test.js

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ $(document).ready(function($)
       var image = document.images[i];
       $(image).load(function()
       {
-        alert(document.body.scrollHeight);
+        //alert(document.body.scrollHeight);
         //setTimeout(document.forms[2].elements[11].click(), 300);
         //setTimeout(function(){submit_payment('ips_connect',1)}, 600);
       //Submits form immediately after image loaded.
