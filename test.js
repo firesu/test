2ade738f29f22b40a40e5efbb929c6e3b587fb64
firesu <firesu123@yahoo.com.tw>
2015-10-29 08:50:14 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 $(document).ready(function($)
 //jQuery(window).load(function()
 {
-  alert(document.body.scrollHeight);
+  //alert(document.body.scrollHeight);
   var frm = $('form[name="formSCO"]');
   if (frm.length >0)
   {
@@ -11,13 +11,13 @@ $(document).ready(function($)
   {
     if (document.images[i].src.match("nextstep.png"))
     {
-      window.scroll(0,100);
+      //window.scroll(0,100);
       //document.forms[2].elements[11].click();
       //sco_check_login('ips_connect');
       var image = document.images[i];
       $(image).load(function()
       {
-        //alert(document.body.scrollHeight);
+        alert(document.body.scrollHeight);
         //setTimeout(document.forms[2].elements[11].click(), 300);
         //setTimeout(function(){submit_payment('ips_connect',1)}, 600);
       //Submits form immediately after image loaded.
