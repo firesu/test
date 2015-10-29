4afd8ed3d2a13b55e1f7e27bf75e179ae762d713
firesu <firesu123@yahoo.com.tw>
2015-10-29 11:34:26 +0800

    Update myfone_1.js

--- myfone_1.js
+++ myfone_1.js
@@ -10,8 +10,8 @@ $(document).ready(function($)
       if (document.body.scrollHeight>6560)
      {
       //alert(document.body.scrollHeight);
-      setTimeout(document.forms[2].elements[11].click(), 300);
-      setTimeout(function(){submit_payment('ips_connect',1)}, 800);
+      setTimeout(document.forms[2].elements[11].click(), 200);
+      setTimeout(function(){submit_payment('ips_connect',1)}, 500);
       clearInterval(checkExist);
      }
     }, 100);
