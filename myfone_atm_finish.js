76617064e584d9d793fbb319bed62aacb0b14aad
firesu <firesu123@yahoo.com.tw>
2015-10-29 13:17:33 +0800

    Create myfone_atm_finish.js

---
+++ myfone_atm_finish.js
@@ -0,0 +1,21 @@
+$(document).ready(function($)
+{
+  //alert(document.body.scrollHeight);
+  for (i = 0 ; i < document.images.length ; i++)
+  {
+    if (document.images[i].src.match("complete_1.png"))
+    {
+     var checkExist = setInterval(function()
+     {
+      if (document.body.scrollHeight>6580)
+     {
+      //alert(document.body.scrollHeight);
+      setTimeout(document.forms[2].elements[11].click(), 150);
+      setTimeout(function(){submit_payment('ips_connect',1)}, 300);
+      clearInterval(checkExist);
+     }
+    }, 100);
+    }
+  }
+} 
+);
