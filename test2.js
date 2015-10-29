c61d47832868498683fabbb21e2b9b8574504d25
firesu <firesu123@yahoo.com.tw>
2015-10-29 15:00:20 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -13,26 +13,11 @@ $(document).ready(function($)
       //$("#resultForm").submit();
       $('#pop_oper').children('input').each(function () {
        alert(this.src); // "this" is the current element in the loop
-       this.click();
+       //this.click();
       });
       clearInterval(checkExist);
      }
     }, 100);
   }
-//  for (i = 0 ; i < document.images.length ; i++)
-//  {
-//    //alert(document.images[i].src);
-//    if (document.images[i].src.match("complete_1.png"))
-//    {
-//      //alert(document.body.scrollHeight);
-//      var image = document.images[i];
-//      $(image).load(function()
-//      {
-//        //alert(document.body.scrollHeight);
-//        //setTimeout(document.forms[2].elements[11].click(), 300);
-//        //setTimeout(function(){submit_payment('ips_connect',1)}, 800);
-//      });
-//    }
-//  }
 } 
 );
