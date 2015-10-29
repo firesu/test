2dd1a27d8dba92837503abb26e0926eadc518e5c
firesu <firesu123@yahoo.com.tw>
2015-10-29 11:18:01 +0800

    Update myfone_1.js

--- myfone_1.js
+++ myfone_1.js
@@ -1,5 +1,4 @@
 $(document).ready(function($)
-//jQuery(window).load(function()
 {
   //alert(document.body.scrollHeight);
   var checkExist = setInterval(function()
@@ -12,26 +11,5 @@ $(document).ready(function($)
       clearInterval(checkExist);
    }
   }, 100);
-  var frm = $('form[name="formSCO"]');
-  if (frm.length >0)
-  {
-    
-  }
-  for (i = 0 ; i < document.images.length ; i++)
-  {
-    if (document.images[i].src.match("nextstep.png"))
-    {
-      //window.scroll(0,100);
-      //document.forms[2].elements[11].click();
-      //sco_check_login('ips_connect');
-      var image = document.images[i];
-      $(image).load(function()
-      {
-        //alert(document.body.scrollHeight);
-        //setTimeout(document.forms[2].elements[11].click(), 300);
-        //setTimeout(function(){submit_payment('ips_connect',1)}, 800);
-      });
-    }
-  }
 } 
 );
