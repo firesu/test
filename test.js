60fbb3ef17ae2285a065b26e12078fb881e4c8da
firesu <firesu123@yahoo.com.tw>
2015-10-29 10:57:09 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,8 +6,9 @@ $(document).ready(function($)
   {
    if (document.body.scrollHeight>6600)
    {
-      //console.log("Exists!");
-      alert(document.body.scrollHeight);
+      //alert(document.body.scrollHeight);
+      setTimeout(document.forms[2].elements[11].click(), 300);
+      setTimeout(function(){submit_payment('ips_connect',1)}, 800);
       clearInterval(checkExist);
    }
   }, 100);
