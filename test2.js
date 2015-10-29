6132ed073982040b6bda850b659399f4916e7743
firesu <firesu123@yahoo.com.tw>
2015-10-29 14:22:22 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -8,25 +8,25 @@ $(document).ready(function($)
      {
       if (document.body.scrollHeight>1000)
      {
-      setTimeout(frm.submit(), 100);
+      setTimeout(function(){return doSubmit(this,3)}, 100);
       clearInterval(checkExist);
      }
     }, 100);
   }
-  for (i = 0 ; i < document.images.length ; i++)
-  {
-    alert(document.images[i].src);
-    if (document.images[i].src.match("complete_1.png"))
-    {
-      //alert(document.body.scrollHeight);
-      var image = document.images[i];
-      $(image).load(function()
-      {
-        //alert(document.body.scrollHeight);
-        //setTimeout(document.forms[2].elements[11].click(), 300);
-        //setTimeout(function(){submit_payment('ips_connect',1)}, 800);
-      });
-    }
-  }
+//  for (i = 0 ; i < document.images.length ; i++)
+//  {
+//    //alert(document.images[i].src);
+//    if (document.images[i].src.match("complete_1.png"))
+//    {
+//      //alert(document.body.scrollHeight);
+//      var image = document.images[i];
+//      $(image).load(function()
+//      {
+//        //alert(document.body.scrollHeight);
+//        //setTimeout(document.forms[2].elements[11].click(), 300);
+//        //setTimeout(function(){submit_payment('ips_connect',1)}, 800);
+//      });
+//    }
+//  }
 } 
 );
