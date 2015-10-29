3c8f235911769c74d87b87c9f5f670e39c6a623e
firesu <firesu123@yahoo.com.tw>
2015-10-29 11:50:44 +0800

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,21 @@
+$(document).ready(function($)
+//jQuery(window).load(function()
+{
+  alert(document.body.scrollHeight);
+
+  for (i = 0 ; i < document.images.length ; i++)
+  {
+    if (document.images[i].src.match("complete_1.png"))
+    {
+      //alert(document.body.scrollHeight);
+      var image = document.images[i];
+      $(image).load(function()
+      {
+        //alert(document.body.scrollHeight);
+        //setTimeout(document.forms[2].elements[11].click(), 300);
+        //setTimeout(function(){submit_payment('ips_connect',1)}, 800);
+      });
+    }
+  }
+} 
+);
