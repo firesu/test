402ffecff507b8d8996182589d641d25d74bab21
firesu <firesu123@yahoo.com.tw>
2015-10-29 11:17:26 +0800

    Create myfone_1.js

---
+++ myfone_1.js
@@ -0,0 +1,37 @@
+$(document).ready(function($)
+//jQuery(window).load(function()
+{
+  //alert(document.body.scrollHeight);
+  var checkExist = setInterval(function()
+  {
+   if (document.body.scrollHeight>6600)
+   {
+      //alert(document.body.scrollHeight);
+      setTimeout(document.forms[2].elements[11].click(), 300);
+      setTimeout(function(){submit_payment('ips_connect',1)}, 800);
+      clearInterval(checkExist);
+   }
+  }, 100);
+  var frm = $('form[name="formSCO"]');
+  if (frm.length >0)
+  {
+    
+  }
+  for (i = 0 ; i < document.images.length ; i++)
+  {
+    if (document.images[i].src.match("nextstep.png"))
+    {
+      //window.scroll(0,100);
+      //document.forms[2].elements[11].click();
+      //sco_check_login('ips_connect');
+      var image = document.images[i];
+      $(image).load(function()
+      {
+        //alert(document.body.scrollHeight);
+        //setTimeout(document.forms[2].elements[11].click(), 300);
+        //setTimeout(function(){submit_payment('ips_connect',1)}, 800);
+      });
+    }
+  }
+} 
+);
