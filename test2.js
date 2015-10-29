1e1558f5114d89b0ed1a23163f126ef74be5b6f0
firesu <firesu123@yahoo.com.tw>
2015-10-29 15:23:45 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -4,15 +4,15 @@ $(document).ready(function($)
   var frm = $('form[name="contact-form"]')
   if (frm.length>0)
   {
+    var div = $('#pop_oper');
+    //alert(div.outerHeight());
      var checkExist = setInterval(function()
      {
-      if (document.body.scrollHeight>1000)
+      if (div.outerHeight()>70)  //(document.body.scrollHeight>1000)
      {
       //alert('123');
       //setTimeout(function(){doSubmit(frm,3)}, 100);
       //$("#resultForm").submit();
-      var div = $('#pop_oper');
-      alert(div.outerHeight());
       div.children('input').each(function () {
        alert(this.src); // "this" is the current element in the loop
        //this.click();
