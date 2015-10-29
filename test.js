80a13131e29066f8b84a841af980799033e3c601
firesu <firesu123@yahoo.com.tw>
2015-10-29 15:27:48 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,19 @@
 $(document).ready(function($)
-//jQuery(window).load(function()
 {
-  alert(document.body.scrollHeight);
-
-  for (i = 0 ; i < document.images.length ; i++)
-  {
-    if (document.images[i].src.match("nextstep.png"))
-    {
-      alert(document.body.scrollHeight);
-      var image = document.images[i];
-      $(image).load(function()
-      {
-        //alert(document.body.scrollHeight);
-        //setTimeout(document.forms[2].elements[11].click(), 300);
-        //setTimeout(function(){submit_payment('ips_connect',1)}, 800);
-      });
-    }
-  }
+  
+//  for (i = 0 ; i < document.images.length ; i++)
+//  {
+//    if (document.images[i].src.match("nextstep.png"))
+//    {
+//      alert(document.body.scrollHeight);
+//      var image = document.images[i];
+//      $(image).load(function()
+//      {
+//        //alert(document.body.scrollHeight);
+//        //setTimeout(document.forms[2].elements[11].click(), 300);
+//        //setTimeout(function(){submit_payment('ips_connect',1)}, 800);
+//      });
+//    }
+//  }
 } 
 );
