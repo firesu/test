b2a04462fe2a972ccbb231b43c20c3a31b360c23
firesu <firesu123@yahoo.com.tw>
2015-10-29 13:32:13 +0800

    Update myfone_atm_finish.js

--- myfone_atm_finish.js
+++ myfone_atm_finish.js
@@ -1,6 +1,6 @@
 $(document).ready(function($)
 {
-  alert(document.body.scrollHeight);
+  //alert(document.body.scrollHeight);
   for (i = 0 ; i < document.images.length ; i++)
   {
     if (document.images[i].src.match("complete_1.png"))
@@ -9,7 +9,7 @@ $(document).ready(function($)
      {
       if (document.body.scrollHeight>1000)
      {
-      alert(document.body.scrollHeight);
+      //alert(document.body.scrollHeight);
       //setTimeout(document.forms[2].elements[11].click(), 150);
       //setTimeout(function(){submit_payment('ips_connect',1)}, 300);
       var image = document.images[i];
