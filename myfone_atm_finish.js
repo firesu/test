fe8c52dbf0cde562d18df2ea039340e67dfd2ce2
firesu <firesu123@yahoo.com.tw>
2015-10-30 11:00:53 +0800

    Update myfone_atm_finish.js

--- myfone_atm_finish.js
+++ myfone_atm_finish.js
@@ -1,19 +1,22 @@
-$(document).ready(function($)
+$(function()
 {
   var frm = $('form[name="contact-form"]')
   if (frm.length>0)
   {
+     var div = $('#pop_oper');
      var checkExist = setInterval(function()
      {
-      if (document.body.scrollHeight>1000)
+      //if (document.body.scrollHeight>1000)
+      if (div.length>0)
      {
       $('#pop_oper').children('input').each(function () {
        //alert(this.src); // "this" is the current element in the loop
-       this.click();
+       //this.click();
+       setTimeout(this.click(), 100);
       });
       clearInterval(checkExist);
      }
-    }, 100);
+    }, 50);
   }
 } 
 );
